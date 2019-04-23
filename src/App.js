26ca51f94ef8f83d22aac4a619b3791cdc1fd5bc
Varun <varun.khurana@vanderbilt.edu>
2019-04-22 21:13:30 -0400

    Staging my committ again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload this app.
+          Edit <code>src/App.js</code> and save to reload testing out my app A.
         </p>
         <a
           className="App-link"
